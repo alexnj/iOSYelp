e1f32b16ae7702ce32f4d9187af74b5428d2e17f
Alex Jose <alexnj@gmail.com>
2014-06-21 16:44:28 -0700

    Thumbnails and rating images loaded.

--- Yelp/TableViewCell.m
+++ Yelp/TableViewCell.m
@@ -11,6 +11,7 @@
 @interface TableViewCell ()
 @property (weak, nonatomic) IBOutlet UILabel *address;
 @property (weak, nonatomic) IBOutlet UIImageView *thumbnail;
+@property (weak, nonatomic) IBOutlet UIImageView *ratingImage;
 @property (weak, nonatomic) IBOutlet UILabel *title;
 @property (weak, nonatomic) IBOutlet UILabel *distance;
 @property (weak, nonatomic) IBOutlet UILabel *expense;
@@ -33,6 +34,35 @@
 - (void)setRow:(NSDictionary *)row {
     self.title.text = row[@"name"];
     self.address.text = row[@"location"][@"address"][0];
+    
+    [NSURLConnection sendAsynchronousRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:row[@"image_url"]]] queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
+        
+        UIImage *image = [UIImage imageWithData:data];
+        
+        [self setImageOnMainThread:self.thumbnail image:image];
+    }];
+    
+    [NSURLConnection sendAsynchronousRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:row[@"rating_img_url"]]] queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
+        
+        UIImage *image = [UIImage imageWithData:data];
+        
+        [self setImageOnMainThread:self.ratingImage image:image];
+    }];
 }
 
+- (void)setImageOnMainThread: (UIImageView *)imageView image:(UIImage *)image
+{
+    if (!image)
+        return;
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        CATransition *transition = [CATransition animation];
+        transition.type = kCATransitionFade;
+        transition.duration = 0.5;
+        transition.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+        [imageView.layer addAnimation:transition forKey:nil];
+        
+        imageView.image = image;
+    });
+}
 @end
