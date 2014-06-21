c4785b171ef11174ca0013926d2d7c42fd4392d7
Alex Jose <alexnj@gmail.com>
2014-06-21 13:16:46 -0700

    Adds APi secrets back.

---
+++ Yelp/YelpApiSecrets.h
@@ -0,0 +1,17 @@
+//
+//  YelpApiSecrets.h
+//  Yelp
+//
+//  Created by Alex on 6/21/14.
+//  Copyright (c) 2014 alexnj. All rights reserved.
+//
+
+#ifndef yelp_secrets
+#define yelp_secrets
+
+NSString* const kYelpConsumerKey    = @"";
+NSString* const kYelpConsumerSecret = @"";
+NSString* const kYelpToken          = @"";
+NSString* const kYelpTokenSecret    = @"";
+
+#endif
